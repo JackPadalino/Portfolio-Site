a4abe9b87411f65ffbf0afd913da16926e8abc1a
JackPadalino <pada0867@gmail.com>
2023-06-02 18:40:15 -0400

    added svg image to Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.css";
+import { ReactComponent as Banner } from "./banner.svg";
 
 const Home = () => {
   return (
     <div id="home-container">
-      <h1>Home</h1>
+      <Banner />
     </div>
   );
 };
