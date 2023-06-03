766b501acc0d6a61195f79fdc2a09c7e505b6541
JackPadalino <pada0867@gmail.com>
2023-06-03 09:30:48 -0400

    updated fonts for home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,7 +6,14 @@ const Home = () => {
   return (
     <div id="home-container">
       <Banner />
-      <h1 id="home-name">Jack Padalino</h1>
+      <div id="home-info">
+        <h1 className="home-info" id="home-name">
+          Jack Padalino
+        </h1>
+        <p className="home-info" id="home-title">
+          Full-Stack Developer
+        </p>
+      </div>
     </div>
   );
 };
