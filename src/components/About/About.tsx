e1943460a494e9a4f5bb862773e2c7827f145d14
JackPadalino <pada0867@gmail.com>
2023-06-04 23:46:42 -0400

    added svg graphics to About component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,16 @@
 import "./About.css";
+import halfMoon from "../../../graphics/50satomic-08.svg";
+import starBurst from "../../../graphics/50satomic-04.svg";
 
 const About = () => {
   return (
     <div id="about-container">
-      <h1>About</h1>
+      {/* <h1>About</h1> */}
+      <div id="graphics-container">
+        <img src={starBurst} id="starburst1" />
+        <img src={halfMoon} id="halfMoon" />
+        <img src={starBurst} id="starburst2" />
+      </div>
     </div>
   );
 };
