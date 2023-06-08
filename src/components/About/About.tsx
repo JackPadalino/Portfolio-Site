fb271ec087d8ddfe1a19e81251a1619eb1bbd068
JackPadalino <pada0867@gmail.com>
2023-06-08 10:55:03 -0400

    using intersection observer and css animations to make hello sign move right on user scroll

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,16 +1,48 @@
-import "./About.css";
+import { useRef, useEffect } from "react";
 import helloSign from "../../../images/graphics/helloSign2.png";
 import background from "../../../images/graphics/background2.jpg";
+import "./About.css";
 
 const About = () => {
+  const signRef = useRef<HTMLImageElement>(null);
+
+  const signCallback = (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("startLeft");
+      entry.target.classList.add("slideRight");
+      observer.unobserve(entry.target);
+    }
+  };
+
+  const signOptions = {
+    root: null,
+    threshold: 0.25,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(signCallback, signOptions);
+    if (signRef.current) {
+      observer.observe(signRef.current as Element);
+    }
+  }, [signCallback]);
+
   return (
-    <div id="about-main-container">
-      <div className="about-container" id="left-container">
-        <img src={helloSign} id="helloSign" />
-        <img src={background} id="about-background" />
+    <div id="aboutMainContainer">
+      <div className="aboutContainer" id="leftContainer">
+        <img
+          ref={signRef}
+          src={helloSign}
+          className="startLeft"
+          id="helloSign"
+        />
+        <img src={background} id="aboutBackground" />
       </div>
-      <div className="about-container" id="right-container">
-        <img src={background} id="about-background" />
+      <div className="aboutContainer" id="rightContainer">
+        <img src={background} id="aboutBackground" />
       </div>
     </div>
   );
