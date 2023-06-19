42f3437023a47b5c19e27a331dfdf32424982234
JackPadalino <pada0867@gmail.com>
2023-06-19 14:33:05 -0400

    added "driveInSign" graphic and adjust for mobile views

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,9 +1,16 @@
+import { useRef, useEffect } from "react";
+import driveInSign from "../../../images/graphics/driveInSign.png";
 import "./Projects.css";
 
 const Projects = () => {
   return (
-    <div id="projects-container">
-      <h1>Projects</h1>
+    <div id="projectsMainContainer">
+      <div id="projectsLeftContainer">
+        <img id="driveInSign" src={driveInSign} />
+      </div>
+      <div id="projectsRightContainer">
+        <h1>Projects</h1>
+      </div>
     </div>
   );
 };
