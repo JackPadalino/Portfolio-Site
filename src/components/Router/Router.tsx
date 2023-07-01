b296d87b1737a482979914bb15f7d5d72342234c
JackPadalino <pada0867@gmail.com>
2023-07-01 12:48:36 -0400

    added carousel branch and component

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { App, Home, Projects, Resume, Contact, Error } from "..";
+import { App, Home, Projects, Resume, Contact, Error, Carousel } from "..";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/carousel" element={<Carousel />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
