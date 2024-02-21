af5c8c5104b09c7538d27db17dc3f89179cb3bd3
JackPadalino <pada0867@gmail.com>
2024-02-21 13:18:29 -0500

    updated styling for about component for better spacing on mobile

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -33,30 +33,25 @@ const About = () => {
 
   return (
     <div id="aboutMainContainer">
-      <div className="aboutContainer">
-        <img
-          ref={meRef}
-          src={me}
-          id="meImg"
-          className={
-            aboutMe ? "about_me about_me-left" : "about_me about_me-left-start"
-          }
-        />
-      </div>
-      <div className="aboutContainer">
-        <p
-          id="aboutMeText"
-          className={
-            aboutMe
-              ? "about_me about_me-right"
-              : "about_me about_me-right-start"
-          }
-        >
-          Pleasure to meet you! I'm Jack, a full stack software engineer eager
-          to take on exciting and complex projects alongside a talented and
-          supportive team.
-        </p>
-      </div>
+      <img
+        ref={meRef}
+        src={me}
+        id="meImg"
+        className={
+          aboutMe ? "about_me about_me-left" : "about_me about_me-left-start"
+        }
+      />
+
+      <p
+        id="aboutMeText"
+        className={
+          aboutMe ? "about_me about_me-right" : "about_me about_me-right-start"
+        }
+      >
+        Pleasure to meet you! I'm Jack, a full stack software engineer eager to
+        take on exciting and complex projects alongside a talented and
+        supportive team.
+      </p>
     </div>
   );
 };
