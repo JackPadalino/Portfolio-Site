72fe449457ffe5e02d5593e5bb272ae9c1e797c8
JackPadalino <pada0867@gmail.com>
2024-02-21 11:50:42 -0500

    updated class names and css file for Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,12 +3,7 @@ import { useAppDispatch } from "../../store/hooks";
 import { setNavVisible } from "../../store/navSlice";
 import { ReactComponent as Banner } from "../../../images/graphics/banner.svg";
 import linkedIn1 from "../../../images/graphics/linkedIn1.png";
-import linkedIn2 from "../../../images/graphics/linkedIn2.png";
-import linkedIn3 from "../../../images/graphics/linkedIn3.png";
-import linkedIn4 from "../../../images/graphics/linkedIn4.png";
-import github1 from "../../../images/graphics/github1.png";
 import github2 from "../../../images/graphics/github2.png";
-import email from "../../../images/graphics/email.png";
 import "./Home.css";
 
 const Home = () => {
@@ -28,21 +23,14 @@ const Home = () => {
     threshold: 0,
   };
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(stickyNavCallback, navOptions);
-  //   if (homeRef.current) {
-  //     observer.observe(homeRef.current as Element);
-  //   }
-  // }, [stickyNavCallback]);
-
   return (
-    <div ref={homeRef} id="home-container">
+    <div ref={homeRef} id="homeMainContainer">
       <Banner />
-      <div id="home-info">
-        <p className="home-info" id="home-name">
+      <div id="homeInfoContainer">
+        <p className="homeInfoText" id="homeName">
           Jack Padalino
         </p>
-        <p className="home-info" id="home-title">
+        <p className="homeInfoText" id="homeTitle">
           Full-Stack Developer
         </p>
         <div id="iconsDiv">
@@ -55,9 +43,6 @@ const Home = () => {
           <a href="https://github.com/JackPadalino" target="_blank">
             <img className="icon" src={github2} />
           </a>
-          {/* <a href="mailto: pada0867@gmail.com">
-            <img className="icon" src={email} />
-          </a> */}
         </div>
       </div>
     </div>
