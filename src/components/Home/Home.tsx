5df340573e1b852e14a9d9f9a084488677783300
JackPadalino <pada0867@gmail.com>
2024-10-22 10:54:28 -0400

    added some framer motion animations to home screen

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,50 +1,56 @@
-import { useRef, useEffect } from "react";
-import { useAppDispatch } from "../../store/hooks";
-import { setNavVisible } from "../../store/navSlice";
-import { ReactComponent as Banner } from "../../../images/graphics/banner.svg";
-import linkedIn1 from "../../../images/graphics/linkedIn1.png";
-import github2 from "../../../images/graphics/github2.png";
-import "./Home.css";
+import { motion } from 'framer-motion';
+import { ReactComponent as Banner } from '../../../images/graphics/banner.svg';
+import linkedIn1 from '../../../images/graphics/linkedIn1.png';
+import github2 from '../../../images/graphics/github2.png';
+import './Home.css';
 
 const Home = () => {
-  // Set up for intersection observer and making nav visible on user scroll.
-  // If this component is no longer in view, nav will be set to visible using
-  // Redux store.
-  const dispatch = useAppDispatch();
-  const homeRef = useRef<HTMLDivElement>(null);
-
-  const stickyNavCallback = (entries: IntersectionObserverEntry[]) => {
-    const entry = entries[0];
-    dispatch(setNavVisible(!entry.isIntersecting));
-  };
-
-  const navOptions = {
-    root: null,
-    threshold: 0,
-  };
-
   return (
-    <div ref={homeRef} id="homeMainContainer">
+    <div id="homeMainContainer">
       <Banner />
-      <div id="homeInfoContainer">
+      <motion.div
+        id="homeInfoContainer"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+      >
         <p className="homeInfoText" id="homeName">
           Jack Padalino
         </p>
-        <p className="homeInfoText" id="homeTitle">
+        <motion.p
+          className="homeInfoText"
+          id="homeTitle"
+          animate={{
+            rotate: [0, -45, 360],
+          }}
+          transition={{ delay: 0.75, duration: 0.75 }}
+        >
           Full-Stack Developer
-        </p>
+        </motion.p>
         <div id="iconsDiv">
           <a
             href="https://www.linkedin.com/in/jack-padalino-5b743826/"
             target="_blank"
           >
-            <img className="icon" src={linkedIn1} />
+            <motion.img
+              className="icon"
+              src={linkedIn1}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.75, duration: 0.75 }}
+            />
           </a>
           <a href="https://github.com/JackPadalino" target="_blank">
-            <img className="icon" src={github2} />
+            <motion.img
+              className="icon"
+              src={github2}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 2.25, duration: 0.75 }}
+            />
           </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
