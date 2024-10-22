fc1b66567724074d3863b22a76c8ba91971b39c4
JackPadalino <pada0867@gmail.com>
2024-10-22 12:17:47 -0400

    added new animation to homeName text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,28 +5,77 @@ import github2 from '../../../images/graphics/github2.png';
 import './Home.css';
 
 const Home = () => {
+  const nameArr = [
+    'J',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'P',
+    'a',
+    'd',
+    'a',
+    'l',
+    'i',
+    'n',
+    'o',
+  ];
+
+  const nameVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  const letterVariants = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  // generate an array of random delays for the homeName text
+  const randomDelays = Array.from(
+    { length: nameArr.length },
+    () => Math.random() * 3
+  );
+
   return (
     <div id="homeMainContainer">
       <Banner />
-      <motion.div
-        id="homeInfoContainer"
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <p className="homeInfoText" id="homeName">
-          Jack Padalino
-        </p>
+      <div id="homeInfoContainer">
+        <motion.div
+          id="homeNameContainer"
+          variants={nameVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {nameArr.map((letter, index) => (
+            <motion.p
+              className="homeName"
+              key={index}
+              variants={letterVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: randomDelays[index] }} // choose a random delay to apply to each letter
+            >
+              {letter === ' ' ? '\u00A0' : letter}{' '}
+            </motion.p>
+          ))}
+        </motion.div>
         <motion.p
           className="homeInfoText"
           id="homeTitle"
           animate={{
             rotate: [0, -45, 360],
           }}
-          transition={{ delay: 0.75, duration: 0.75 }}
+          transition={{ delay: 3, duration: 0.75 }}
         >
           Full-Stack Developer
         </motion.p>
+
         <div id="iconsDiv">
           <a
             href="https://www.linkedin.com/in/jack-padalino-5b743826/"
@@ -37,7 +86,7 @@ const Home = () => {
               src={linkedIn1}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ delay: 1.75, duration: 0.75 }}
+              transition={{ delay: 4, duration: 0.75 }}
             />
           </a>
           <a href="https://github.com/JackPadalino" target="_blank">
@@ -46,11 +95,11 @@ const Home = () => {
               src={github2}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ delay: 2.25, duration: 0.75 }}
+              transition={{ delay: 4.25, duration: 0.75 }}
             />
           </a>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
