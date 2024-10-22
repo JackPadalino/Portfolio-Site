7c92e9744e80d89dbffb5ca8e05f2196530ecccb
JackPadalino <pada0867@gmail.com>
2024-10-22 10:54:51 -0400

    removed redux and Nav components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,9 @@
-import {
-  Nav,
-  Home,
-  About,
-  DriveIn,
-  Projects,
-  Resume,
-  Contact,
-  Footer,
-} from "..";
-import "./App.css";
+import { Home, About, DriveIn, Projects, Resume, Contact, Footer } from '..';
+import './App.css';
 
 const App = () => {
   return (
     <div id="component-container">
-      <Nav />
       <Home />
       <About />
       <DriveIn />
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import store from "./store/store";
-import { App, Router } from "./components";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { App, Router } from './components';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <Router />
-      </Provider>
+      <Router />
     </BrowserRouter>
   </React.StrictMode>
 );
