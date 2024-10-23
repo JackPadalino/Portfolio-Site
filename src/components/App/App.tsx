8018b6e9642d2fc7af0ff1ba3d0f67eb85cbbf6f
JackPadalino <pada0867@gmail.com>
2024-10-23 02:13:25 -0400

    began adding logic and styliing for parallax effect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,18 @@
 import { Home, About, DriveIn, Projects, Resume, Contact, Footer } from '..';
+import { ReactComponent as Banner } from '../../../images/graphics/banner.svg';
 import './App.css';
 
 const App = () => {
   return (
-    <div id="component-container">
+    <div id="appContainer">
+      <img
+        src="../../../images/graphics/background3.jpg"
+        id="appContainerImg"
+      />
       <Home />
       <About />
       <DriveIn />
       <Projects />
-      {/* <Resume />
-      <Contact /> */}
       <Footer />
     </div>
   );
