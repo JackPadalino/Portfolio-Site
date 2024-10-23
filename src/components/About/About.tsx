5ba9b1ff2dd83aba5711afe5144671d4950abbb1
JackPadalino <pada0867@gmail.com>
2024-10-23 00:45:15 -0400

    began adding logic to use framer motion for animations on about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,57 +1,71 @@
-import { useRef, useState, useEffect } from "react";
-import me from "../../../images/pictures/me3.jpg";
-import "./About.css";
+import { useRef, useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import me from '../../../images/pictures/me3.jpg';
+import './About.css';
 
 const About = () => {
   const meRef = useRef<HTMLImageElement>(null);
   const [aboutMe, setAboutMe] = useState(false);
 
-  const observerCallback = (
-    entries: IntersectionObserverEntry[],
-    observer: IntersectionObserver
-  ) => {
-    const entry = entries[0];
-    if (entry.isIntersecting) {
-      if (entry.target.classList.contains("about_me")) {
-        setAboutMe(true);
-      }
-      observer.unobserve(entry.target);
-    }
-  };
+  // const observerCallback = (
+  //   entries: IntersectionObserverEntry[],
+  //   observer: IntersectionObserver
+  // ) => {
+  //   const entry = entries[0];
+  //   if (entry.isIntersecting) {
+  //     if (entry.target.classList.contains('about_me')) {
+  //       setAboutMe(true);
+  //     }
+  //     observer.unobserve(entry.target);
+  //   }
+  // };
 
-  const meOptions = {
-    root: null,
-    threshold: 0.8,
-  };
+  // const meOptions = {
+  //   root: null,
+  //   threshold: 0.8,
+  // };
+
+  // useEffect(() => {
+  //   const meObserver = new IntersectionObserver(observerCallback, meOptions);
+  //   if (meRef.current) {
+  //     meObserver.observe(meRef.current as Element);
+  //   }
+  // }, [observerCallback]);
+
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.4, // Percentage of the image visible before triggering (0.1 = 10%)
+  });
 
   useEffect(() => {
-    const meObserver = new IntersectionObserver(observerCallback, meOptions);
-    if (meRef.current) {
-      meObserver.observe(meRef.current as Element);
+    if (inView) {
+      controls.start({ x: 0, scale: 1, opacity: 1 }); // Start animation when in view
     }
-  }, [observerCallback]);
+  }, [inView, controls]);
 
   return (
     <div id="aboutMainContainer">
-      <img
-        ref={meRef}
+      <motion.img
+        ref={ref}
         src={me}
         id="meImg"
-        className={
-          aboutMe ? "about_me about_me-left" : "about_me about_me-left-start"
-        }
+        initial={{ x: -300, scale: 0.8, opacity: 0 }}
+        transition={{ type: 'spring', duration: 2 }}
       />
 
-      <p
+      <motion.p
+        ref={ref}
         id="aboutMeText"
-        className={
-          aboutMe ? "about_me about_me-right" : "about_me about_me-right-start"
-        }
+        initial={{ x: 300, scale: 0.8, opacity: 0 }}
+        animate={controls}
+        transition={{ type: 'spring', duration: 2, opacity: 1 }}
       >
         Pleasure to meet you! I'm Jack, a full stack software engineer eager to
         take on exciting and complex projects alongside a talented and
         supportive team.
-      </p>
+      </motion.p>
     </div>
   );
 };
