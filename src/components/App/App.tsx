60d2908bdf01e1c8e6eaf990987578beef0bb08f
JackPadalino <pada0867@gmail.com>
2024-10-25 02:41:20 -0400

    began adding contact section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
       <div id="componentContainer">
         <Home />
         <About />
+        <Contact />
         {/* <DriveIn /> */}
         {/* <Projects /> */}
         <Footer />
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,14 @@
-import "./Contact.css";
+import './Contact.css';
 
 const Contact = () => {
-  return <div id="contact-container"></div>;
+  return (
+    <div id="contactMainContainer">
+      <p id="contactMeText">
+        Let's connect to explore how I can bring my passion for innovation and
+        tackling complex problems to your team.
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
