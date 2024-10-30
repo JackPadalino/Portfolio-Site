472f431a9b3afd2a1a575bcc78e41fae4652baa6
JackPadalino <pada0867@gmail.com>
2024-10-29 20:00:34 -0400

    removed icons from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,12 @@
-import linkedIn1 from "../../../images/graphics/linkedIn1.png";
-import github2 from "../../../images/graphics/github2.png";
-import "./Footer.css";
+import linkedIn1 from '../../../images/graphics/linkedIn1.png';
+import github2 from '../../../images/graphics/github2.png';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <div id="footerMainContainer">
       <p id="footerText">Copyright 2023 Jack Padalino. All rights reserverd.</p>
-      <div id="iconsDiv">
+      {/* <div id="iconsDiv">
         <a
           href="https://www.linkedin.com/in/jack-padalino-5b743826/"
           target="_blank"
@@ -16,7 +16,7 @@ const Footer = () => {
         <a href="https://github.com/JackPadalino" target="_blank">
           <img className="icon" src={github2} />
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
